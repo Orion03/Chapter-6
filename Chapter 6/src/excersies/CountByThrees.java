ae6878de6ea07c7f428004031ffa9837a8f67835
JackReis01 <jackreis2019@gmail.com>
2018-10-01 09:14:47 -0500

    updated text

--- Chapter 6/src/excersies/CountByThrees.java
+++ Chapter 6/src/excersies/CountByThrees.java
@@ -5,9 +5,14 @@ public class CountByThrees {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i = 0; i <= 10; i = i +1)
+		for(int i = 0; i <= 10; i = i++)
 		{
 			System.out.println("Count " + i);
+		}
+		
+		for(int j = 10; j >= 0; j--)
+		{
+			System.out.print("Count " + j);
 		}
 
 	}
